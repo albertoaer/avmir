852f8ba7e64a6ad235e553fabbd8a115e14a0233
Alberto Elorza <albertoelorza2002@gmail.com>
2024-05-24 19:00:35 +0200

    memory mapped files as memory

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use std::{env::args, fs};
+use std::{env::args, fs::{self, OpenOptions}, io};
 
-use vm::{machine::MachineBuilder, memory::MemoryBuffer};
+use memmap2::MmapOptions;
+use thiserror::Error;
+use vm::machine::MachineBuilder;
 
 use crate::{parser::{Parser, simple}, vm::machine::Machine};
 
 mod vm;
 mod parser;
 
-fn main() {
-    let mut machine: Machine = MachineBuilder::new().add_memory(MemoryBuffer::new(1024)).build();
+#[derive(Debug, Error)]
+enum RuntimeError {
+  #[error("io error: {0}")]
+  Fs(#[from] io::Error)
+}
 
-    for file in args().skip(1) {
-        let content = fs::read_to_string(file).expect("expecting file content");
-        let program = match simple::Simple::parse(content) {
-            Ok(program) => program,
-            Err(err) => panic!("{}", err)
-        };
-        machine.launch(program);
-    }
+fn main() -> Result<(), RuntimeError> {
+  let file = OpenOptions::new().read(true).write(true).create(true).open("./mem.bin")?;
+  file.set_len(1024)?;
 
-    machine.wait();
+  let mapped = unsafe { MmapOptions::new().map_mut(&file)? };
+
+  let mut machine: Machine = MachineBuilder::new().add_memory(mapped).build();
+
+  for file in args().skip(1) {
+    let content = fs::read_to_string(file).expect("expecting file content");
+    let program = match simple::Simple::parse(content) {
+      Ok(program) => program,
+      Err(err) => panic!("{}", err)
+    };
+    machine.launch(program);
+  }
+
+  machine.wait();
+
+  Ok(())
 }
--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -1,3 +1,5 @@
+use std::ops::DerefMut;
+
 pub trait Memory: Send + Sync {
   fn write_int_64(&mut self, int: i64, offset: usize);
   fn read_int_64(&self, offset: usize) -> i64;
@@ -40,5 +42,15 @@ impl Memory for MemoryBuffer {
 
   fn read_int_64(&self, offset: usize) -> i64 {
     i64::from_le_bytes(self.raw[offset..8].try_into().unwrap())
+  }
+}
+
+impl<T> Memory for T where T : DerefMut<Target = [u8]> + Send + Sync {
+  fn write_int_64(&mut self, int: i64, offset: usize) {
+    self[offset..8].copy_from_slice(&int.to_le_bytes());
+  }
+
+  fn read_int_64(&self, offset: usize) -> i64 {
+    i64::from_le_bytes(self[offset..8].try_into().unwrap())
   }
 }
