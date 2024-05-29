0bc4d5b8da9d3febaecbcbaae9f5b175e89e8b2c
Alberto Elorza <albertoelorza2002@gmail.com>
2024-05-29 16:20:08 +0200

    update version

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,9 +2,11 @@ use std::borrow::BorrowMut;
 
 use crate::vm::program::Program;
 
-#[cfg_attr(not(somefeature), path = "simple_v1.rs")]
+#[path = "simple_v1.rs"]
+#[deprecated(since = "0.1.1", note = "use v2 instead")]
 pub mod v1;
-#[cfg_attr(not(somefeature), path = "simple_v2.rs")]
+
+#[path = "simple_v2.rs"]
 pub mod v2;
 
 pub trait Parser {
