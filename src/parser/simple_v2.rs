962818ded1bdb966e6578593ddfbfea4603e6854
Alberto Elorza <albertoelorza2002@gmail.com>
2024-05-29 14:45:34 +0200

    v2 with all features

--- src/parser/simple_v2.rs
+++ src/parser/simple_v2.rs
@@ -1,3 +1,13 @@
+//! This is the version 2 of the simple parser.
+//! 
+//! It's designed to provide tags in order to write memory access and jumps easier,
+//! 
+//! A line must contain only either: a comment, a chunk to be written in memory or a instruction
+//! 
+//! If no tag is provided, for example "#memory" ":push 0", the index of the line would be used as tag (starting in 0)
+//! 
+//! Numeric tags are not compatible with v1, so both parsers are not interchangeable
+
 use std::{borrow::BorrowMut, collections::HashMap, str::FromStr};
 
 use thiserror::Error;
@@ -13,8 +23,11 @@ pub enum InternalSimpleParserError {
   #[error("operand invalid syntax")]
   OperandInvalidSyntax,
 
-  #[error("bad memory item: {0}")]
-  BadMemoryItem(usize),
+  #[error("bad tag: {0}, not found")]
+  BadTagNotFound(String),
+
+  #[error("bad tag: {0}, only $ is valid for instruction tag")]
+  InstructionBadTag(String),
 
   #[error("bad line syntax: {0}")]
   BadLineSyntax(String),
@@ -27,30 +40,64 @@ pub enum InternalSimpleParserError {
 #[error("Error [LINE: {0}] :: {1}")]
 pub struct SimpleParserError(usize, InternalSimpleParserError);
 
+enum Tag {
+  Memory {
+    address: usize,
+    size: usize,
+  },
+  Instruction {
+    line: usize
+  }
+}
+
 struct ParserV2<'a> {
   program: &'a mut Program,
-  memory_tags: HashMap<String, usize>, // tag => memory address
-  line_tags: HashMap<String, usize>, // tag => command
+  tags: HashMap<String, Tag>, // tag => command
 }
 
 impl<'a> ParserV2<'a> {
   pub fn consume_tags_and_memory(&mut self, source: &mut Vec<(usize, String)>) -> Result<(), SimpleParserError> {
-    source.retain(|(_, x)| match x.trim() {
-      ";" | "" => false,
+    source.retain(|(_, x)| match x.trim().chars().next() {
+      Some(';') | None => false,
       _ => true
     }); // remove empty lines and comments
 
-    for (idx, line) in source.iter_mut().map(|(idx, line)| (idx, line.trim_start())) {
-      match (line.find(";"), line.find(":")) {
+    let mut instruction_counter = 0;
+
+    for (idx, line) in source.iter_mut() {
+      *line = line.trim_start().into();
+
+      if let Some(memory_idx) = line.find("#") {
+        let tag: String = match line[..memory_idx].trim() {
+          x if x.len() > 0 => x.into(),
+          _ => idx.to_string()
+        };
+
+        let begin = self.program.static_data.len();
+        self.program.static_data.extend_from_slice(&line[(memory_idx + 1)..].as_bytes());
+        let end = self.program.static_data.len();
+        self.program.static_data_meta.push((begin, end - begin)); // TODO?: remove static_data_meta
+
+        self.tags.insert(tag, Tag::Memory { address: begin, size: end - begin });
+        
+        *line = String::new();
+
+        continue // skip so the instruction counter does not increment
       }
 
-      if line.starts_with("#") {
-        let begin = self.program.static_data.len();
-        self.program.static_data.extend_from_slice(&line.as_bytes()[1..]);
-        let end = self.program.static_data.len();
-        self.program.static_data_meta.push((begin, end - begin));
-        continue
+      if let Some(line_tag_idx) = line.find(":") {
+        let tag: String = match line[..line_tag_idx].trim() {
+          x if x.len() > 0 => x.into(),
+          _ => idx.to_string()
+        };
+
+        self.tags.insert(tag, Tag::Instruction { line: instruction_counter });
+
+        *line = line[(line_tag_idx+1)..].into();
       }
+
+      // here we are assume this is a instruction, otherwise consume_instructions will return an error
+      instruction_counter += 1; 
     }
 
     source.retain(|(_, x)| !x.trim().is_empty()); // remove empty lines
@@ -79,17 +126,17 @@ impl<'a> ParserV2<'a> {
   }
 
   pub fn parse_operand(&mut self, item: &str) -> Result<Option<InstructionParam>, InternalSimpleParserError> {
-    // get memory item address, size or next address
+    // handle tag
     if item.starts_with("$") || item.starts_with("@") || item.starts_with("^") {
-      return if let Ok(idx) = item[1..].parse() {
-        match (item.chars().next().unwrap(), self.program.static_data_meta.get(idx)) {
-          ('$', Some((address, _))) => Ok(Some(InstructionParam::Int(*address as i64))),
-          ('@', Some((_, size))) => Ok(Some(InstructionParam::Int(*size as i64))),
-          (_, Some((address, size))) => Ok(Some(InstructionParam::Int((*address + *size) as i64))),
-          (_, None) => Err(InternalSimpleParserError::BadMemoryItem(idx))
-        }
-      } else {
-        Err(InternalSimpleParserError::OperandInvalidSyntax)
+      let tag = &item[1..];
+      return match (item.chars().next().unwrap(), self.tags.get(tag)) {
+        ('$', Some(&Tag::Memory { address, .. })) => Ok(Some(InstructionParam::Int(address as i64))),
+        ('@', Some(&Tag::Memory { size, .. })) => Ok(Some(InstructionParam::Int(size as i64))),
+        ('^', Some(&Tag::Memory { address, size })) => Ok(Some(InstructionParam::Int((address + size) as i64))),
+        ('$', Some(&Tag::Instruction { line })) => Ok(Some(InstructionParam::Int(line as i64))),
+        ('@' | '^', Some(&Tag::Instruction { .. })) => Err(InternalSimpleParserError::InstructionBadTag(tag.into())),
+        (_, None) => Err(InternalSimpleParserError::BadTagNotFound(tag.into())),
+        _ => Err(InternalSimpleParserError::BadTagNotFound(tag.into())) // should never trigger, char not in $, @, ^
       }
     }
 
@@ -112,8 +159,7 @@ impl Parser for Simple {
   fn parse(mut target: impl BorrowMut<Program>, source: impl AsRef<str>) -> Result<(), Self::Err> {
     let mut parser = ParserV2{
       program: target.borrow_mut(),
-      line_tags: HashMap::new(),
-      memory_tags: HashMap::new()
+      tags: HashMap::new()
     };
 
     let mut source: Vec<_> = source.as_ref().lines().enumerate().map(|(idx, line)| (idx, line.to_string())).collect();
